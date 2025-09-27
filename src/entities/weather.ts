317c4d7130595e485c95262576be404b5bc9cf46
m.j.a.lucky <m.javad.aghajanian@gmail.com>
2025-09-27 18:18:59 +0330

    feat: - complete weather entity

--- src/entities/weather.ts
+++ src/entities/weather.ts
@@ -2,9 +2,33 @@ import { Entity, PrimaryGeneratedColumn, Column } from "typeorm"
 
 @Entity()
 export class Weather {
-    @PrimaryGeneratedColumn()
+    @PrimaryGeneratedColumn("uuid")
     id!: number
 
+    @Column("varchar", { unique: true })
+    cityName!: string
+
+    @Column("varchar")
+    country!: string
+
+    @Column("float")
+    temperature!: number
+    
     @Column()
-    test!: string
+    description!: string
+
+    @Column()
+    humidity!: number
+
+    @Column("float")
+    windSpeed!: number
+
+    @Column({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
+    fetchedAt!: Date
+
+    @Column({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
+    createdAt!: Date
+
+    @Column({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP', onUpdate: 'CURRENT_TIMESTAMP' })
+    updatedAt!: Date
 }
